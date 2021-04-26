301245523eade23c2df2a890f0ecf66d6612ab15
bosciu <bosciu@icloud.com>
2021-04-26 14:51:46 +0200

    Richista nome utente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert("Ciao")
+/* Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+var nomeUtente = prompt("Come ti chiami?");
+console.log(nomeUtente);
