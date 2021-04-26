107f79ad8b53aa9c01eaa36ff481ed64775c8a78
bosciu <bosciu@icloud.com>
2021-04-26 14:52:46 +0200

    Richista cognome

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 
-var nomeUtente = prompt("Come ti chiami?");
+var nomeUtente = prompt("Qual'è il tuo nome?");
 console.log(nomeUtente);
+
+var cognomeUtente = prompt("Qual'è il tuo cognome?")
+console.log(cognomeUtente);
