b8f30e6e44a8bdea7e227e00db5ba76dea1f108c
bosciu <bosciu@icloud.com>
2021-04-26 15:04:37 +0200

    Rimosso Console.log di debug

--- js/script.js
+++ js/script.js
@@ -4,12 +4,9 @@ poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 
 var nomeUtente = prompt("Qual'è il tuo nome?");
-console.log(nomeUtente);
 
 var cognomeUtente = prompt("Qual'è il tuo cognome?")
-console.log(cognomeUtente);
 
 var colorePreferito = prompt("Qual'è il tuo colore preferito?")
-console.log(colorePreferito);
 
-document.getElementById("root").innerHTML = "Ciao " + nomeUtente + ", la password generata per te è: " + nomeUtente + cognomeUtente + colorePreferito + "21"
+document.getElementById("root").innerHTML = "Ciao " + nomeUtente + ", la password generata per te è: " + nomeUtente + cognomeUtente + colorePreferito + "21";
